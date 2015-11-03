f30336e1f10d860ab974b3665348aa69b5fe5a09
Jake Coltman <=>
2015-11-03 13:27:13 +0000

    Added event stream interface to handle events from the API

---
+++ MarketInvoiceAlert/MarketInvoiceAlert/IEventStream.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketInvoiceAlert
+{
+    interface IEventStream
+    {
+        bool add_handler(IEventHandler handler);
+        bool check_for_updates(); 
+    }
+}
