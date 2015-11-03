4f4fe588f8977c66f180db74af078ab16333603c
Jake Coltman <=>
2015-11-03 13:28:10 +0000

    Added event handler

---
+++ MarketInvoiceAlert/MarketInvoiceAlert/IEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketInvoiceAlert
+{
+    interface IEventHandler
+    {
+        bool can_handle(Message message);
+        bool handle(Message message)
+    }
+}
