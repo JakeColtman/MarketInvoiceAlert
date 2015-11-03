f2f9f0ab3a5031ae8810cf4102b9ee8249a00072
Jake Coltman <=>
2015-11-03 16:57:07 +0000

    Added telegram event handler

---
+++ MarketInvoiceAlert/MarketInvoiceAlert/Telegram/TelegramEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarketInvoiceAlert;
+
+namespace MarketInvoiceAlert.Telegram
+{
+    class TelegramEventHandler : IEventHandler
+    {
+        ITelegramAPI _api;
+
+        public TelegramEventHandler(ITelegramAPI api)
+        {
+            _api = api;
+        }
+
+        public bool can_handle(Message message)
+        {
+            return true;
+        }
+
+        public bool handle(Message message)
+        {
+            NewPaymentMessage paymentMsg = message as NewPaymentMessage;
+            _api.send_message("Payment message recieved");
+            return true;
+        }
+    }
+}
