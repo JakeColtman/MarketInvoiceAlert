768587607a65cab866e3cba8238f5587488d3a3e
Jake Coltman <=>
2015-11-03 13:28:58 +0000

    Added abstract base class for messages

---
+++ MarketInvoiceAlert/MarketInvoiceAlert/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketInvoiceAlert
+{
+    abstract class Message
+    {
+        //Basic message class - extend this to create different message types
+    }
+}
