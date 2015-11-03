46db29f82d991bd6c3248ef821e58ac4c825ce72
Jake Coltman <jakecoltman@sky.com>
2015-11-03 13:37:37 +0000

    Added mock handler

---
+++ MarketInvoiceAlert/MarketInvoiceAlert/MockEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketInvoiceAlert
+{
+    class MockEventHandler : IEventHandler
+    {
+        public bool can_handle(Message message)
+        {
+            return true;
+        }
+
+        public bool handle(Message message)
+        {
+            Console.WriteLine("Sent message");
+            return true;
+        }
+    }
+}
